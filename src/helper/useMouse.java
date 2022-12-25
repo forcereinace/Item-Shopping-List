4fa9242b5a1b3e2e4f7708bf50e418f4be59a6cd
Amirul Akmal <amirul.akmal.putrajaya2003@gmail.com>
2022-12-25 17:48:35 +0800

    Helping

---
+++ src/helper/useMouse.java
@@ -0,0 +1,31 @@
+package src.helper;
+
+import javax.swing.JComponent;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class useMouse 
+{
+    public useMouse(JComponent component)
+    {
+        component.addMouseMotionListener(new MouseMotionListener()
+        {
+            @Override
+            public void mouseDragged(MouseEvent e) 
+            {
+                int x = e.getXOnScreen();
+                int y = e.getYOnScreen();
+                component.setLocation(x,y);
+                System.out.println("x: " + x + " y: " + y);
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) 
+            {
+                System.out.println("Mouse Moved");
+            }
+        });
+    }
+}
+
